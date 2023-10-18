d86d3230a8400529a12fd1fd4b895433e4796a2f
Gimi818 <gimi8183@gmail.com>
2023-10-18 22:32:56 +0200

    Added Film entity

---
+++ src/main/java/com/cinema/film/Film.java
@@ -0,0 +1,27 @@
+package com.cinema.film;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "film")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Film {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    private FilmCategory category;
+
+    private int durationFilmInMinutes;
+}
