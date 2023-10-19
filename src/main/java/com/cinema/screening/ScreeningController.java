c3125c59bbc3917fdee75e614dd45d712f758695
Gimi818 <gimi8183@gmail.com>
2023-10-19 22:03:13 +0200

    Added post method  saveScreening

--- src/main/java/com/cinema/screening/ScreeningController.java
+++ src/main/java/com/cinema/screening/ScreeningController.java
@@ -1,6 +1,8 @@
 package com.cinema.screening;
 
+import com.cinema.screening.dto.ScreeningRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +17,7 @@ public class ScreeningController {
     private final ScreeningService service;
 
     @PostMapping("/add")
-    public ResponseEntity<Screening> saveScreening(@RequestBody ScreeningRequestDto screeningDto){
-        return new ResponseEntity<>(service.save)
+    public ResponseEntity<Screening> saveScreening(@RequestBody ScreeningRequestDto screeningDto) {
+        return new ResponseEntity<>(service.saveScreening(screeningDto), HttpStatus.CREATED);
     }
 }
