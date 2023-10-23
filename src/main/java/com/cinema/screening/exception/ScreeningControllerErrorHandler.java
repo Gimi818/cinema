030bc44e746b54ae6eba49642c2c9dda3139ffe3
Gimi818 <gimi8183@gmail.com>
2023-10-23 19:45:05 +0200

    Added Error Handler To Screening

---
+++ src/main/java/com/cinema/screening/exception/ScreeningControllerErrorHandler.java
@@ -0,0 +1,26 @@
+package com.cinema.screening.exception;
+
+import com.cinema.film.exception.FilmErrorResponse;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@ControllerAdvice
+@Log4j2
+public class ScreeningControllerErrorHandler {
+
+
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ScreeningNotFoundException.class)
+    @ResponseBody
+    public FilmErrorResponse ScreeningNotFound(ScreeningNotFoundException exception) {
+        final String message = exception.getMessage();
+        log.error(message);
+        return new
+                FilmErrorResponse(message, HttpStatus.NOT_FOUND);
+    }
+}
