704cceeb377aa2f2be9791418d9143922cab887c
Gimi818 <gimi8183@gmail.com>
2023-10-23 14:04:04 +0200

    Added  new request to FilmRepository

--- src/main/java/com/cinema/film/FilmRepository.java
+++ src/main/java/com/cinema/film/FilmRepository.java
@@ -1,7 +1,17 @@
 package com.cinema.film;
 
+import com.cinema.film.filmCategory.FilmCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 public interface FilmRepository extends JpaRepository<Film,Long> {
+
+
+    @Query("SELECT f FROM Film f WHERE f.category = :category")
+    List<Film> findByCategory(@Param("category") FilmCategory category);
+
+    boolean existsByTitle(String title);
 }
 
