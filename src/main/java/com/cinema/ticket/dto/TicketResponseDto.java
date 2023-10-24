d0ceb8173b052c3bf4916d3ac1eab3ffdde557b7
Gimi818 <gimi8183@gmail.com>
2023-10-24 15:14:46 +0200

    added TicketDto

---
+++ src/main/java/com/cinema/ticket/dto/TicketResponseDto.java
@@ -0,0 +1,15 @@
+package com.cinema.ticket.dto;
+
+import com.cinema.ticket.TicketStatus;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record TicketResponseDto( TicketStatus status,
+
+         String filmTitle,
+         LocalDate screeningDate,
+         LocalTime screeningTime,
+
+         String cinemaRoomId) {
+}
