07e3a5f96ffb9b1c59923a310fb2d309562604e5
Gimi818 <gimi8183@gmail.com>
2023-10-24 15:15:21 +0200

    update methods in ScreeningControllerErrorHandler

--- src/main/java/com/cinema/screening/exception/ScreeningControllerErrorHandler.java
+++ src/main/java/com/cinema/screening/exception/ScreeningControllerErrorHandler.java
@@ -3,6 +3,7 @@ package com.cinema.screening.exception;
 import com.cinema.film.exception.FilmErrorResponse;
 
 import com.cinema.film.exception.FilmNotFoundException;
+import com.cinema.screening.Screening;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,20 +17,20 @@ public class ScreeningControllerErrorHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ScreeningNotFoundByIdException.class)
     @ResponseBody
-    public FilmErrorResponse ScreeningNotFoundByIdException(ScreeningNotFoundByIdException exception) {
+    public ScreeningErrorResponse ScreeningNotFoundByIdException(ScreeningNotFoundByIdException exception) {
         final String message = exception.getMessage();
         log.error(message);
         return new
-                FilmErrorResponse(message, HttpStatus.NOT_FOUND);
+                ScreeningErrorResponse(message, HttpStatus.NOT_FOUND);
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ScreeningNotFoundException.class)
     @ResponseBody
-    public FilmErrorResponse ScreeningNotFound(ScreeningNotFoundException exception) {
+    public ScreeningErrorResponse ScreeningNotFound(ScreeningNotFoundException exception) {
         final String message = exception.getMessage();
         log.error(message);
         return new
-                FilmErrorResponse(message, HttpStatus.NOT_FOUND);
+                ScreeningErrorResponse(message, HttpStatus.NOT_FOUND);
     }
 }
