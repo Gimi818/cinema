708672ce2c0c999113678929adc5c6b3a7478d9b
Gimi818 <gimi8183@gmail.com>
2023-10-25 20:07:57 +0200

    chance name exception

--- src/main/java/com/cinema/film/exception/FilmExistByTitleException.java
+++ src/main/java/com/cinema/film/exception/FilmExistByTitleException.java
@@ -2,6 +2,6 @@ package com.cinema.film.exception;
 
 public class FilmExistByTitleException  extends  RuntimeException{
     public FilmExistByTitleException(String title){
-        super(String.format("Wrong film category '%s' ",title));
+        super(String.format("The film of this name '%s' is exists ",title));
     }
 }
