93dff4d71b1801265e48bf9901732fd4329b0fc0
Gimi818 <gimi8183@gmail.com>
2023-10-25 18:29:35 +0200

    Code refactoring in ScreeningService

--- src/main/java/com/cinema/screening/ScreeningService.java
+++ src/main/java/com/cinema/screening/ScreeningService.java
@@ -5,7 +5,6 @@ import com.cinema.film.FilmRepository;
 import com.cinema.film.exception.FilmNotFoundException;
 import com.cinema.screening.dto.ScreeningRequestDto;
 import com.cinema.screening.dto.ScreeningResponseDto;
-import com.cinema.screening.exception.ScreeningNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -24,13 +23,14 @@ public class ScreeningService {
     private final ScreeningRepository repository;
     private final ScreeningMapper mapper;
     private final FilmRepository filmRepository;
+    private final ScreeningValidate validate;
 
     @Transactional
     public Screening saveScreening(ScreeningRequestDto screeningRequestDto, Long filmId) {
         log.info("Saving Screening {}", screeningRequestDto);
         Film film = filmRepository.findById(filmId).orElseThrow(() -> new FilmNotFoundException(filmId));
+        validate.dataValidation(screeningRequestDto);
         Screening screening = repository.save(mapper.dtoToEntity(screeningRequestDto));
-        screening.setFilm(film);
         log.info("Saved Screening ");
         return screening;
     }
@@ -45,9 +45,7 @@ public class ScreeningService {
 
     @Transactional
     public List<ScreeningResponseDto> getScreeningsByDate(LocalDate date) {
-        if (!repository.existsByDate(date)) {
-            throw new ScreeningNotFoundException(date);
-        }
+        validate.checkCorrectData(date);
         List<Screening> screenings = repository.findScreeningsByDate(date);
         log.info("Found {} screenings.", screenings.size());
         log.info("Returning all screenings by date {}", date);
