cb2a91b11772846648ad4b15cb6f92b3b17bb94b
Gimi818 <gimi8183@gmail.com>
2023-10-27 23:35:46 +0200

    Added first name and last name to Registration

--- src/main/java/com/cinema/user/dto/UserRequestDto.java
+++ src/main/java/com/cinema/user/dto/UserRequestDto.java
@@ -8,6 +8,10 @@ import jakarta.validation.constraints.Size;
 
 public record UserRequestDto(
         @NotBlank
+        String firstName,
+        @NotBlank
+        String lastName,
+        @NotBlank
         @Email
         String email,
         @NotBlank
@@ -16,7 +20,7 @@ public record UserRequestDto(
 
         @NotBlank
         @Size(min = 6, max = 20)
-        String repeatedPassword ,
+        String repeatedPassword,
 
 
         UserRole role
