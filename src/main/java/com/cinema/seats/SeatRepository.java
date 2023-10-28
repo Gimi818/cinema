19d0080f464c82ee6388966124254b5901aa8921
Gimi818 <gimi8183@gmail.com>
2023-10-28 16:06:57 +0200

    Added annotation @Repository

---
+++ src/main/java/com/cinema/seats/SeatRepository.java
@@ -0,0 +1,10 @@
+package com.cinema.seats;
+
+import com.cinema.screening.Screening;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+    Seat findByScreeningAndRowsNumberAndSeatInRow(Screening screening, int rowsNumber, int seatInRow);
+}
