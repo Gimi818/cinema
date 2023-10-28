d33ad6d38f9a6a21f28829a1efa9a754edd3c732
Gimi818 <gimi8183@gmail.com>
2023-10-28 16:11:13 +0200

    Ceated new method createSeats and updated saveScreening method

--- src/main/java/com/cinema/screening/ScreeningService.java
+++ src/main/java/com/cinema/screening/ScreeningService.java
@@ -1,5 +1,9 @@
 package com.cinema.screening;
 
+import com.cinema.screening.dto.ScreeningAvailableSeats;
+import com.cinema.seats.Seat;
+import com.cinema.seats.SeatService;
+import com.cinema.seats.SeatStatus;
 import com.cinema.film.Film;
 import com.cinema.film.FilmRepository;
 import com.cinema.film.exception.FilmNotFoundException;
@@ -13,6 +17,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 @Service
@@ -24,18 +29,29 @@ public class ScreeningService {
     private final ScreeningMapper mapper;
     private final FilmRepository filmRepository;
     private final ScreeningValidate validate;
+    private final SeatService seatService;
 
     @Transactional
     public Screening saveScreening(ScreeningRequestDto screeningRequestDto, Long filmId) {
         log.info("Saving Screening {}", screeningRequestDto);
         Film film = filmRepository.findById(filmId).orElseThrow(() -> new FilmNotFoundException(filmId));
         validate.dataValidation(screeningRequestDto);
+
         Screening screening = repository.save(mapper.dtoToEntity(screeningRequestDto));
         screening.setFilm(film);
+        screening.setSeats(createSeats());
         log.info("Saved Screening ");
         return screening;
     }
 
+
+    private List<Seat> createSeats() {
+        return IntStream.rangeClosed(1, 10)
+                .boxed()
+                .flatMap(rowNumber -> IntStream.rangeClosed(1, 10)
+                        .mapToObj(seatInRow -> seatService.createSeat(rowNumber, seatInRow, SeatStatus.AVAILABLE)))
+                .collect(Collectors.toList());
+    }
 
     public List<ScreeningResponseDto> findAllScreenings() {
         log.info("Returning all screenings");
@@ -54,5 +70,18 @@ public class ScreeningService {
                 .map(mapper::entityToDto)
                 .collect(Collectors.toList());
     }
+
+    public ScreeningAvailableSeats findAvailableSeats(Long id) {
+        Screening screening = repository.findScreeningByIdWithSeats(id);
+
+        return mapper.entityToDto2(screening);
+    }
+
+//    public ClientResponseDto findClientById(Long id) {
+//        log.info("Finding client with ID {}", id);
+//        Client client = clientRepository.findById(id).orElseThrow(() -> new ClientNotFoundException(id));
+//        log.info("Found client {}", client);
+//        return clientMapper.entityToDto(client);
+//    }
 
 }
