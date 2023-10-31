8da77d10b018084dbcb4107491939e4c06b6c36f
Gimi818 <gimi8183@gmail.com>
2023-10-31 15:28:12 +0100

    Added new requests to database in UserRepository

--- src/main/java/com/cinema/user/UserRepository.java
+++ src/main/java/com/cinema/user/UserRepository.java
@@ -3,9 +3,12 @@ package com.cinema.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+    User findByConfirmationToken(String confirmationToken);
 }
+
