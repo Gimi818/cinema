91c0f2033f7d74f2ffdc93de47acae176674e0af
Gimi818 <gimi8183@gmail.com>
2023-11-06 09:53:21 +0100

    Added SeatServiceTest

---
+++ src/test/java/com/cinema/seats/SeatServiceTest.java
@@ -0,0 +1,105 @@
+package com.cinema.seats;
+
+
+import com.cinema.screening.Screening;
+import com.cinema.screening.ScreeningRepository;
+import com.cinema.seats.exception.SeatAlreadyTakenException;
+import com.cinema.seats.exception.SeatNotFoundException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class SeatServiceTest {
+
+    @Mock
+    private ScreeningRepository screeningRepository;
+
+    @Mock
+    private SeatRepository seatRepository;
+
+    @InjectMocks
+    private SeatService seatService;
+
+    @Test
+    @DisplayName("Should take the available seat")
+    void should_take_available_seat() {
+        // Given
+        Long screeningId = 1L;
+        int rowsNumber = 1;
+        int seatInRow = 1;
+        Screening screening = new Screening();
+        Seat seat = new Seat();
+        seat.setStatus(SeatStatus.AVAILABLE);
+
+        when(screeningRepository.findById(screeningId)).thenReturn(Optional.of(screening));
+        when(seatRepository.findByScreeningAndRowsNumberAndSeatInRow(screening, rowsNumber, seatInRow)).thenReturn(seat);
+
+        // When
+        seatService.checkSeatsAvailability(screeningId, rowsNumber, seatInRow);
+
+        // Then
+        assertEquals(SeatStatus.TAKEN, seat.getStatus());
+    }
+
+    @Test
+    @DisplayName("Should throw SeatAlreadyException when seat is taken")
+    void should_throw_seatAlreadyException() {
+        // Given
+        Long screeningId = 1L;
+        int rowsNumber = 1;
+        int seatInRow = 1;
+        Screening screening = new Screening();
+        Seat seat = new Seat();
+        seat.setStatus(SeatStatus.TAKEN);
+
+        when(screeningRepository.findById(screeningId)).thenReturn(Optional.of(screening));
+        when(seatRepository.findByScreeningAndRowsNumberAndSeatInRow(screening, rowsNumber, seatInRow)).thenReturn(seat);
+
+        // Then
+        assertThrows(SeatAlreadyTakenException.class, () ->
+                seatService.checkSeatsAvailability(screeningId, rowsNumber, seatInRow));
+    }
+
+    @Test
+    @DisplayName("Should throw SeatNotFoundException when seat  doesn't exist")
+    void should_throw_seatNotFoundException() {
+        // Given
+        Long screeningId = 1L;
+        int rowsNumber = 4;
+        int seatInRow = 3;
+        Screening screening = new Screening();
+
+        when(screeningRepository.findById(screeningId)).thenReturn(Optional.of(screening));
+        when(seatRepository.findByScreeningAndRowsNumberAndSeatInRow(screening, rowsNumber, seatInRow)).thenReturn(null);
+
+        // Then
+        assertThrows(SeatNotFoundException.class, () ->
+                seatService.checkSeatsAvailability(screeningId, rowsNumber, seatInRow));
+    }
+
+    @Test
+    @DisplayName("Should create new seats")
+    void should_create_new_seats() {
+        // Given
+        int rowNumber = 5;
+        int seatInRow = 5;
+        SeatStatus status = SeatStatus.AVAILABLE;
+
+        // When
+        Seat seat = seatService.createSeat(rowNumber, seatInRow, status);
+
+        // Then
+        assertEquals(rowNumber, seat.getRowsNumber());
+        assertEquals(seatInRow, seat.getSeatInRow());
+        assertEquals(status, seat.getStatus());
+    }
+}
