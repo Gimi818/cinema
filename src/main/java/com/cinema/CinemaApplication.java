340ad3db428c4b2721083d8d45ff74ebc6fb280d
Gimi818 <gimi8183@gmail.com>
2023-11-06 19:33:07 +0100

    Added annotation  @EnableScheduling

--- src/main/java/com/cinema/CinemaApplication.java
+++ src/main/java/com/cinema/CinemaApplication.java
@@ -2,8 +2,10 @@ package com.cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CinemaApplication {
 
     public static void main(String[] args) {
