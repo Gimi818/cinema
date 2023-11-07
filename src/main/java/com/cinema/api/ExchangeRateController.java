3394a8e9bdcc5f08f2bf6adb5a54a1362a277979
Gimi818 <gimi8183@gmail.com>
2023-11-07 15:15:17 +0100

    Added findAllCurrencies method

--- src/main/java/com/cinema/api/ExchangeRateController.java
+++ src/main/java/com/cinema/api/ExchangeRateController.java
@@ -1,5 +1,6 @@
 package com.cinema.api;
 
+import com.cinema.api.dto.ExchangeRateResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,17 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/rate")
+@RequestMapping("/codes")
 public class ExchangeRateController {
-
     private final ExchangeRateService service;
 
     @GetMapping()
-    public ResponseEntity<Void> saveEuroRate() {
-        service.requestToNBP();
-        return ResponseEntity.status(HttpStatus.OK).build();
+    public ResponseEntity<List<ExchangeRateResponseDto>> findAllCurrencies() {
+        List<ExchangeRateResponseDto> allCurrencies = service.findAllCurrency();
+        return ResponseEntity.status(HttpStatus.OK).body(allCurrencies);
     }
-
 }
