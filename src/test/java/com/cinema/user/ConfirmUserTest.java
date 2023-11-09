9ebeb78dff8ac385878f5a0967f360d59ce72826
Gimi818 <gimi8183@gmail.com>
2023-11-09 22:24:09 +0100

    Created ConfirmUserTest with unit tests

---
+++ src/test/java/com/cinema/user/ConfirmUserTest.java
@@ -0,0 +1,73 @@
+package com.cinema.user;
+
+import com.cinema.emailSender.ConfirmationEmail;
+import jakarta.mail.MessagingException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class ConfirmUserTest {
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private ConfirmationEmail confirmationEmail;
+    @InjectMocks
+    private ConfirmUser confirmUser;
+
+    @Test
+    @DisplayName("Should generate token")
+    public void should_generate_token() {
+        String token = confirmUser.generateConfirmationToken();
+        assertEquals(36, token.length());
+    }
+    @Test
+    @DisplayName("Should generate confirmation email")
+    public void should_generate_confirmation_email() {
+        User user = new User();
+        user.setConfirmationToken("test_token");
+
+        String confirmationLink = confirmUser.generateConfirmationLink(user);
+        assertEquals("http://localhost:8080/users/confirm?token=test_token", confirmationLink);
+    }
+
+    @Test
+    @DisplayName("Should send confirmation email")
+    public void should_send_email() throws MessagingException {
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setConfirmationToken("test_token");
+
+        doNothing().when(confirmationEmail).sendConfirmationEmail(user.getEmail(), user.getConfirmationToken());
+        confirmUser.sendConfirmationEmail(user);
+
+        verify(confirmationEmail, times(1)).sendConfirmationEmail("test@example.com", "http://localhost:8080/users/confirm?token=test_token");
+    }
+
+
+
+    @Test
+    @DisplayName("Should confirm account")
+    public void should_confirm_account() {
+        User user = new User();
+        user.setConfirmationToken("test_token");
+
+        when(userRepository.findByConfirmationToken("test_token")).thenReturn(user);
+
+        confirmUser.confirmUserAccount("test_token");
+
+        assertEquals(AccountType.ACTIVE, user.getAccountType());
+        assertEquals(null, user.getConfirmationToken());
+
+        verify(userRepository, times(1)).save(user);
+    }
+
+
+}
