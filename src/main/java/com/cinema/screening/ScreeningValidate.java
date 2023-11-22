8bcdd67e1fce5b720dd28c96aabc39c1c38f0261
Gimi818 <gimi8183@gmail.com>
2023-11-22 17:05:37 +0100

    Added Error Messages

--- src/main/java/com/cinema/screening/ScreeningValidate.java
+++ src/main/java/com/cinema/screening/ScreeningValidate.java
@@ -1,13 +1,15 @@
 package com.cinema.screening;
 
+import com.cinema.common.exception.exceptions.NotFoundException;
+import com.cinema.common.exception.exceptions.TimeDifferenceException;
+import com.cinema.common.exception.exceptions.TooLateException;
+import com.cinema.common.exception.exceptions.TooManyException;
 import com.cinema.film.Film;
 import com.cinema.screening.dto.ScreeningRequestDto;
-import com.cinema.screening.exception.ScreeningNotFoundException;
-import com.cinema.screening.exception.ScreeningTimeDifferenceException;
-import com.cinema.screening.exception.ScreeningTooLateToCreateNew;
-import com.cinema.screening.exception.ScreeningTooManyInOneDayException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import static com.cinema.screening.ScreeningValidate.ErrorMessages.*;
 
 import java.time.Duration;
 import java.time.LocalTime;
@@ -22,7 +24,7 @@ public class ScreeningValidate {
 
     public void checkCorrectData(LocalDate date) {
         if (!repository.existsByDate(date)) {
-            throw new ScreeningNotFoundException(date);
+            throw new NotFoundException(NOT_FOUND_BY_DATE, date);
         }
     }
 
@@ -31,7 +33,7 @@ public class ScreeningValidate {
         if (newScreening.date().isAfter(LocalDate.now())) {
             return;
         } else if (newScreening.date().isBefore(LocalDate.now()) || newScreening.time().isBefore(LocalTime.now())) {
-            throw new ScreeningTooLateToCreateNew();
+            throw new TooLateException(TOO_LATE_TO_CREATE);
         }
     }
 
@@ -41,7 +43,7 @@ public class ScreeningValidate {
         for (Screening existingScreening : screeningsOnSameDay) {
             var timeDifference = Duration.between(existingScreening.getTime(), newScreening.time()).toMinutes();
             if (Math.abs(timeDifference) < film.getDurationFilmInMinutes() + 20) {
-                throw new ScreeningTimeDifferenceException();
+                throw new TimeDifferenceException(TOO_SMALL_TIME_DIFFERENCE);
             }
         }
     }
@@ -49,7 +51,7 @@ public class ScreeningValidate {
     public void checkNumberOfScreeningsDuringDay(ScreeningRequestDto newScreening) {
         List<Screening> screeningsOnSameDay = repository.findScreeningsByDate(newScreening.date());
         if (screeningsOnSameDay.size() >= 5) {
-            throw new ScreeningTooManyInOneDayException();
+            throw new TooManyException(TOO_MANY_SCREENINGS);
         }
 
     }
@@ -61,6 +63,13 @@ public class ScreeningValidate {
 
     }
 
+    static final class ErrorMessages {
+        static final String TOO_LATE_TO_CREATE = "It isn't possible to create new screening earlier than the current time";
+        static final String NOT_FOUND_BY_DATE = "Screening with date %s not found";
+        static final String TOO_MANY_SCREENINGS = "It is impossible to add another screening because there are already five";
+        static final String TOO_SMALL_TIME_DIFFERENCE = "Time difference is too small to start a new screening.";
+
+    }
 }
 
 
