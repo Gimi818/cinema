607c12fe9370a34d6b71266b598b7cafd1a675f2
Gimi818 <gimi8183@gmail.com>
2023-11-22 09:23:07 +0100

    Created AbstractEntity

---
+++ src/main/java/com/cinema/uuidEntity/AbstractEntity.java
@@ -0,0 +1,18 @@
+package com.cinema.uuidEntity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldNameConstants;
+
+@MappedSuperclass
+@Getter
+@NoArgsConstructor
+@FieldNameConstants
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+}
