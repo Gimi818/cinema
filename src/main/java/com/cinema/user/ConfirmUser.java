dbf5a010ee97d0ae0d425ab314a1e6d36d0896c0
Gimi818 <gimi8183@gmail.com>
2023-11-23 11:10:06 +0100

    Changed constructor annotation and added @Value to confirmationLink

--- src/main/java/com/cinema/user/ConfirmUser.java
+++ src/main/java/com/cinema/user/ConfirmUser.java
@@ -3,26 +3,29 @@ package com.cinema.user;
 import com.cinema.emailSender.ConfirmationEmail;
 import com.cinema.user.userEnum.AccountType;
 import jakarta.mail.MessagingException;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
-
+import org.springframework.beans.factory.annotation.Value;
 import java.util.UUID;
+
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Log4j2
 public class ConfirmUser {
     private final UserRepository repository;
     private final ConfirmationEmail confirmationEmail;
-
+    @Value("${confirmation.link}")
+    private String confirmationLink;
 
     public String generateConfirmationToken() {
         return UUID.randomUUID().toString();
     }
-    public String generateConfirmationLink(User user) {
 
-        return "http://localhost:8080/users/confirm?token=" + user.getConfirmationToken();
+    public String generateConfirmationLink(User user) {
+        return confirmationLink + user.getConfirmationToken();
     }
+
     public void sendConfirmationEmail(User user) {
         try {
             confirmationEmail.sendConfirmationEmail(user.getEmail(), generateConfirmationLink(user));
@@ -30,18 +33,16 @@ public class ConfirmUser {
             log.error("Error sending the confirmation email.", e);
         }
     }
+
     public void confirmUserAccount(String token) {
 
-        User user = repository.findByConfirmationToken(token);
-
-        if (user != null) {
+        repository.findByConfirmationToken(token).ifPresent(user -> {
             user.setAccountType(AccountType.ACTIVE);
             user.setConfirmationToken(null);
 
             repository.save(user);
             log.info("Email confirmed");
-        }
+        });
+
     }
-
-
 }
