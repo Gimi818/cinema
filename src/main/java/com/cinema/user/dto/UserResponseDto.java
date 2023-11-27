cf3eeac08fa1f79f5279d7e277f9208b38e694e9
Gimi818 <gimi8183@gmail.com>
2023-11-27 19:29:40 +0100

    Added AccountType to dto

--- src/main/java/com/cinema/user/dto/UserResponseDto.java
+++ src/main/java/com/cinema/user/dto/UserResponseDto.java
@@ -1,10 +1,11 @@
 package com.cinema.user.dto;
 
+import com.cinema.user.userEnum.AccountType;
 import com.cinema.user.userEnum.UserRole;
 
 public record UserResponseDto(String firstName,
                               String lastName,
                               String email,
-                              UserRole role
-                           ) {
+                              AccountType accountType
+) {
 }
