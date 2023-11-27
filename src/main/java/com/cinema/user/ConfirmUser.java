3357966bc2b9932810558bfe03f175759a90973e
Gimi818 <gimi8183@gmail.com>
2023-11-27 19:23:38 +0100

    Changed logger information body

--- src/main/java/com/cinema/user/ConfirmUser.java
+++ src/main/java/com/cinema/user/ConfirmUser.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Value;
+
 import java.util.UUID;
 
 @Component
@@ -18,15 +19,17 @@ class ConfirmUser {
     @Value("${confirmation.link}")
     public String confirmationLink;
 
-    public String generateConfirmationLink(User user,String confirmationLink) {
+    public String generateConfirmationLink(User user, String confirmationLink) {
         return confirmationLink + user.getConfirmationToken();
     }
+
     public String generateConfirmationToken() {
         return UUID.randomUUID().toString();
     }
+
     public void sendConfirmationEmail(User user) {
         try {
-            confirmationEmail.sendConfirmationEmail(user.getEmail(), generateConfirmationLink(user,confirmationLink));
+            confirmationEmail.sendConfirmationEmail(user.getEmail(), generateConfirmationLink(user, confirmationLink));
         } catch (MessagingException e) {
             log.error("Error sending the confirmation email.", e);
         }
@@ -39,7 +42,7 @@ class ConfirmUser {
             user.setConfirmationToken(null);
 
             repository.save(user);
-            log.info("Email confirmed");
+            log.info("Email confirmed {}", user.getEmail());
         });
 
     }
